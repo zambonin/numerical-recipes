28b9a7c961c1da4d085cf8b927980a663db19b96
Gustavo Zambonin <gzmbnn@gmail.com>
2016-03-21 19:25:04 -0300

    chapter_1: incomplete resolutions

---
+++ chapter_1/code/maclaurin.py
@@ -0,0 +1,11 @@
+from functools import reduce
+from math import factorial as fact
+from numpy import float32 as single
+
+n = 4
+x = -.111
+desired = 0.894938748929031
+
+for y in [single(x), x]:
+    obtained = reduce(lambda w, i: w + (y**i)/fact(i), range(n), 0)
+    print("{:.70f}%".format((desired - obtained) / desired))
