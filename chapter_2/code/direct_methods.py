81d8e78526380bbe5058da92c0143e1547275938
Gustavo Zambonin <gzmbnn@gmail.com>
2016-04-03 20:11:30 -0300

    chapter_2: direct methods unfinished code

--- chapter_2/code/direct_methods.py
+++ chapter_2/code/direct_methods.py
@@ -1,23 +1,50 @@
-from numpy import dot
-from copy import deepcopy
-
-
 def naive_gaussian_elim(A, b):
     """Gaussian elimination with backward substitution and no pivoting."""
-    A, b, n = deepcopy(A), deepcopy(b), len(A)
-    assert len(b) == n, "A and b have different sizes."
+    assert len(A) == len(b), "A and b have different sizes."
+    ext = [j + [b[i]] for i, j in enumerate(A)]
+    n = len(ext)
 
-    for k in range(n-1):
-        for i in range(k+1, n):
-            mult = A[i][k] / A[k][k]
-            A[i][k] = mult
-            for j in range(k+1, n):
-                A[i][j] -= mult * A[k][j]
-            b[i] -= mult * b[k]
+    for i in range(n-1):
+        for j in range(i+1, n):
+            mult = ext[j][i] / ext[i][i]
+            ext[j][i] = mult
+            for k in range(i+1, n):
+                ext[j][k] -= mult * ext[i][k]
+            ext[j][n] -= mult * ext[i][n]
+
+    assert ext[n-1][n-1] != 0, "No unique solution exists."
 
     x = [0]*n
     for i in range(n-1, -1, -1):
-        x[i] = (b[i] - dot(A[i][i+1:], x[i+1:])) / A[i][i]
+        subs_sum = sum(ext[i][j] * x[j] for j in range(i+1, n))
+        x[i] = (ext[i][n] - subs_sum) / ext[i][i]
+
+    return x
+
+
+def part_pivot_gaussian_elim(A, b):
+    """Gaussian elimination with backward substitution and partial pivoting."""
+    assert len(A) == len(b), "A and b have different sizes."
+    ext = [j + [b[i]] for i, j in enumerate(A)]
+    n = len(ext)
+
+    for i in range(n-1):
+        ext = partial_pivoting(ext, i, n)
+
+        print(ext)
+        for j in range(i+1, n):
+            mult = ext[j][i] / ext[i][i]
+            ext[j][i] = mult
+            for k in range(i+1, n):
+                ext[j][k] -= mult * ext[i][k]
+            ext[j][n] -= mult * ext[i][n]
+
+    assert ext[n-1][n-1] != 0, "No unique solution exists."
+
+    x = [0]*n
+    for i in range(n-1, -1, -1):
+        subs_sum = sum(ext[i][j] * x[j] for j in range(i+1, n))
+        x[i] = (ext[i][n] - subs_sum) / ext[i][i]
 
     return x
 
@@ -32,3 +59,23 @@ def max_residual(x, A, b):
         r[i] = abs(soma - b[i])
 
     return max(r)
+
+
+def partial_pivoting(mat, k, n):
+    """Changes rows to achieve an useful matrix setup."""
+    col = [i[k] for i in mat]
+    i = col.index(max(col)) + k - 1
+    mat[k], mat[i] = mat[i], mat[k]
+
+    return mat
+
+
+A = [[3, 1, -1], [4, -2, 1], [5, 3, 3]]
+b = [1, -1, 2]
+x = naive_gaussian_elim(A, b)
+print(x)
+
+A = [[0, 1, -1], [4, -2, 1], [0, 2, -2]]
+b = [1, -1, 2]
+x = part_pivot_gaussian_elim(A, b)
+print(x)
