2f82b73bd8c555a47ac6626d86e9abf10bf75153
Gustavo Zambonin <gzmbnn@gmail.com>
2016-04-12 14:19:24 -0300

    chapter_2: tridiagonal algorithm is not iterative

--- chapter_2/code/direct_methods.py
+++ chapter_2/code/direct_methods.py
@@ -63,6 +63,48 @@ def part_pivot_gaussian_elim(A, b):
     return x
 
 
+def tridiag_matrix_alg(A, d):
+    """
+    Check if a matrix is n x n with n > 2 and tridiagonal, and proceed with
+    the Thomas algorithm, a simplified form of Gaussian elimination.
+
+    `b`, `a` and `c` are, respectively, the diagonal, subdiagonal and
+    superdiagonal of the matrix.
+
+    Args:
+        A: the matrix of coefficients.
+        d: the constant terms.
+
+    Returns:
+        x: the solution for the system of equations.
+
+    Raises:
+        ValueError: if the matrix is not tridiagonal, the algorithm shall quit.
+    """
+    indexes = [-len(A)] + list(range(1, len(A)))
+    for row, i in zip(A, indexes):
+        if 0 in row[i-1:i+2] or set(row[i+2:] + row[:i-1]) not in ({0}, set()):
+            raise ValueError("Not a tridiagonal matrix.")
+
+    b, a, c = [A[-1][-1]], [0], [0]
+    for i in range(len(A) - 1):
+        b.insert(i, A[i][i])
+        a.append(A[i+1][i])
+        c.insert(i, A[i][i+1])
+
+    n = len(a)
+    for k in range(1, n):
+        m = a[k] / b[k-1]
+        b[k] -= m*c[k-1]
+        d[k] -= m*d[k-1]
+
+    x = [0] * (n-1) + [d[-1] / b[-1]]
+    for k in range(n-1, -1, -1):
+        x[k-1] = (d[k-1] - c[k-1] * x[k]) / b[k-1]
+
+    return x
+
+
 def max_residual(x, A, b):
     """Largest residual from solving a system of linear equations.
 
